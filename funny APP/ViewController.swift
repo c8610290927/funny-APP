a1ccbeac56861858c55883ff1ecd64f1d1eb9d94
c8610290927 <c8610290927@gmail.com>
2019-05-12 14:51:10 +0800

    Initial Commit

---
+++ funny APP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  funny APP
+//
+//  Created by User20 on 2019/5/12.
+//  Copyright © 2019 ouo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
